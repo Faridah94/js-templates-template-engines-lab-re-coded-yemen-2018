b53499b8d08b06725a271edf85583f65851c6963
Farida <faridah.absi@gmail.com>
2018-11-10 06:43:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ function postComment(){
   let comment =document.getElementById('commentText').value;
    let commentTemplate =
     '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
- 
+
   let templateFn = _.template(commentTemplate);
- 
+
   let commentsDiv = document.getElementById('comments');
- 
- 
+
+
   let templateHTML = templateFn({ comment: comment, commenter: commenter });
- 
- 
+
+
   commentsDiv.innerHTML += templateHTML;
 
 }
